4e6cd15452b2e5fbd2c57d0b692f56416594c8b8
Nuttayos Suvisuttikasame <nuttayossuvisuttikasame@MacBook-Pro-khxngNuttayos.local>
2023-01-21 13:59:09 +0700

    - [fix] revert config db

--- config/config.go
+++ config/config.go
@@ -40,7 +40,7 @@ const (
 
 const (
 	dPort         = 1323
-	dDBConnection = "postgresql://group-3:group-3-pass@database-1.c7bdavepehea.ap-southeast-1.rds.amazonaws.com/group-3-dev"
+	dDBConnection = "postgresql://postgres:password@localhost:5432/banking?sslmode=disable"
 )
 
 func (c *cfg) All() Config {
