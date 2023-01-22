cdda6ccb383ca956d802a65170cb2c94ac65056e
Pramote Siriparp <6210530017@student.tu.ac.th>
2023-01-22 10:25:33 +0700

    add finish tranfer without test

--- transaction/transaction.go
+++ transaction/transaction.go
@@ -45,27 +45,63 @@ func (h *handler) Transfer(c echo.Context) error {
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, Err{Message: err.Error()})
 	}
-	query := "Update pockets set amount = amount-$2, updatedAt = $3 where id = $1"
-	stmt, err := h.db.Prepare(query)
-	if err != nil {
-		return c.JSON(http.StatusBadRequest, Err{Message: err.Error()})
+	// get from source pocket
+	ctx := c.Request().Context()
+	pocket := pocket.Pocket{}
+	var createdAt string
+	var updatedAt string
+	var deletedAt *string
+	if err := h.db.QueryRowContext(ctx, "SELECT * FROM pockets WHERE id = $1", t.SourcePocketID).Scan(
+		&pocket.ID,
+		&pocket.Name,
+		&pocket.Category,
+		&pocket.Amount,
+		&pocket.Goal,
+		&pocket.Currency,
+		&createdAt,
+		&updatedAt,
+		&deletedAt,
+	); err != nil {
+		return c.JSON(http.StatusNotFound, Err{Message: err.Error()})
 	}
-	if _, err := stmt.Exec(t.SourcePocketID, t.Amount, time.Now()); err != nil {
-		return c.JSON(http.StatusInternalServerError, Err{Message: err.Error()})
+
+	pocket.CreatedAt, _ = time.Parse(time.RFC3339, createdAt)
+	pocket.UpdatedAt, _ = time.Parse(time.RFC3339, updatedAt)
+
+	// update to each pocket
+	if pocket.Amount >= t.Amount {
+
+		query := "Update pockets set amount = amount-$2, updatedAt = $3 where id = $1"
+		stmt, err := h.db.Prepare(query)
+		if err != nil {
+			return c.JSON(http.StatusBadRequest, Err{Message: err.Error()})
+		}
+		if _, err := stmt.Exec(t.SourcePocketID, t.Amount, time.Now()); err != nil {
+			return c.JSON(http.StatusInternalServerError, Err{Message: err.Error()})
+		}
+		query = "Update pockets set amount = amount+$2, updatedAt = $3 where id = $1"
+		stmt, err = h.db.Prepare(query)
+		if err != nil {
+			return c.JSON(http.StatusBadRequest, Err{Message: err.Error()})
+		}
+		if _, err := stmt.Exec(t.DestinationPocketID, t.Amount, time.Now()); err != nil {
+			return c.JSON(http.StatusInternalServerError, Err{Message: err.Error()})
+		}
+		query = `INSERT INTO transactions (type, status, amount, sourcePocketId, destinationPocketId, description, currency, createdAt) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id`
+		row := h.db.QueryRow(query, TransferTransactionType, SuccessTransactionStatus, t.Amount, t.SourcePocketID, t.DestinationPocketID, t.Description, t.Currency, time.Now())
+		err = row.Scan(&t.ID)
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, Err{Message: err.Error()})
+		}
+		return c.JSON(http.StatusCreated, t)
+	} else {
+		query := `INSERT INTO transactions (type, status, amount, sourcePocketId, destinationPocketId, description, currency, createdAt) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id`
+		row := h.db.QueryRow(query, TransferTransactionType, FailedTransactionStatus, t.Amount, t.SourcePocketID, t.DestinationPocketID, t.Description, t.Currency, time.Now())
+		err = row.Scan(&t.ID)
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, Err{Message: err.Error()})
+		}
+		return c.JSON(http.StatusBadRequest, Err{Message: "not enough money"})
 	}
-	query = "Update pockets set amount = amount+$2, updatedAt = $3 where id = $1"
-	stmt, err = h.db.Prepare(query)
-	if err != nil {
-		return c.JSON(http.StatusBadRequest, Err{Message: err.Error()})
-	}
-	if _, err := stmt.Exec(t.DestinationPocketID, t.Amount, time.Now()); err != nil {
-		return c.JSON(http.StatusInternalServerError, Err{Message: err.Error()})
-	}
-	query = `INSERT INTO transactions (type, status, amount, sourcePocketId, destinationPocketId, description, currency, createdAt) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id`
-	row := h.db.QueryRow(query, TransferTransactionType, SuccessTransactionStatus, t.Amount, t.SourcePocketID, t.DestinationPocketID, t.Description, t.Currency, time.Now())
-	err = row.Scan(&t.ID)
-	if err != nil {
-		return c.JSON(http.StatusInternalServerError, Err{Message: err.Error()})
-	}
-	return c.JSON(http.StatusCreated, t)
+
 }
